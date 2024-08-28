f831fe1643acadd9922378698de6af59278f3244
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-08-28 13:02:30 -0300

    [add] criando componentes

--- frontend/src/pages/GerenciarSenhas/index.js
+++ frontend/src/pages/GerenciarSenhas/index.js
@@ -1,59 +1,38 @@
-import React from 'react';
-import SideBar from '../../components/SideBar'
+import React from "react";
+import SideBar from "../../components/SideBar";
 
-export default function GerenciarSenhas() {
+export default function GerenciarSenhas(){
 
-    
     return (
         <div className="w-full">
-
             <SideBar
                 title={'GERENCIAR SENHAS'}
-            />
+            />  
 
-            <div className="mx-80 mt-8 p-8 space-y-8">
-                {/* Senha Normal */}
-                <div className="border border-black rounded-lg p-6 flex flex-col space-y-6">
-                    <h2 className="text-center text-black font-semibold">Senha Normal</h2>
-                    <div className="flex justify-center">
-                        <div className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black">
-                            827
-                        </div>
-                    </div>
-                    <div className="flex justify-between">
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Próxima Senha
-                        </h4>
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Senha Anterior
-                        </h4>
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Senha Manual
-                        </h4>
-                    </div>
+            <div className="mx-80 mt-12 space-y-8 justify-center text-center">
+                <div className="justify-center text-center flex">
+                    <h3 className="text-black bg-green-300 p-4 w-60 rounded-lg">Novo Dispositivo</h3>
                 </div>
 
-                {/* Senha Preferencial */}
-                <div className="border border-black rounded-lg p-6 flex flex-col space-y-6">
-                    <h2 className="text-center text-black font-semibold">Senha Preferencial</h2>
-                    <div className="flex justify-center">
-                        <div className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black">
-                            948
-                        </div>
+                <div className="w-full h-70 bg-gray-200 py-8 px-20 rounded-lg">
+                    <div className="justify-between items-center flex">
+                        <h3 className="text-black bg-gray-300 py-4 px-28 rounded-lg">Dispositivo 1</h3>
+                        <h3 className="text-black bg-green-300 py-4 px-10 rounded-lg">Playlist</h3>
+                        <h3 className="text-black bg-red-300 py-4 px-10 rounded-lg">Excluir</h3>
                     </div>
-                    <div className="flex justify-between">
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Próxima Senha
-                        </h4>
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Senha Anterior
-                        </h4>
-                        <h4 className="bg-purple-100 text-black py-2 px-4 rounded-lg border border-black cursor-pointer">
-                            Senha Manual
-                        </h4>
+                    <div className="justify-between items-center flex my-8">
+                        <h3 className="text-black bg-gray-300 py-4 px-28 rounded-lg">Dispositivo 2</h3>
+                        <h3 className="text-black bg-green-300 py-4 px-10 rounded-lg">Playlist</h3>
+                        <h3 className="text-black bg-red-300 py-4 px-10 rounded-lg">Excluir</h3>
+                    </div>
+                    <div className="justify-between items-center flex">
+                        <h3 className="text-black bg-gray-300 py-4 px-28 rounded-lg">Dispositivo 3</h3>
+                        <h3 className="text-black bg-green-300 py-4 px-10 rounded-lg">Playlist</h3>
+                        <h3 className="text-black bg-red-300 py-4 px-10 rounded-lg">Excluir</h3>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
