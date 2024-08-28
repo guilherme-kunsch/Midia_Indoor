1d3a91254ccbc0ad6539493bb3dbf86bab8b0220
joaoovtms <joaovictormoreirasarmento@gmail.com>
2024-08-28 11:43:16 -0300

    deixando barra estática

--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -22,7 +22,7 @@ export default function SideBar({ title }) {
       <div
         className={`${
           open ? "w-72" : "w-20 "
-        } h-screen bg-dark-purple z-10 fixed justify-center text-center duration-300 shadow-md shadow-dark-purple`}
+        } h-screen bg-dark-purple top-0 z-10 fixed justify-center text-center duration-300 shadow-md shadow-dark-purple`}
       >
         <img
           src={control}
@@ -91,8 +91,10 @@ export default function SideBar({ title }) {
         </ul>
       </div>
 
-      <div className="w-full flex justify-center items-center bg-dark-purple text-white py-4 px-6 shadow shadow-dark-purple">
-        <h1 className="text-2xl font-bold">{title}</h1>
+      <div className="w-full fixed top-0 justify-center items-center bg-dark-purple text-white py-4 px-6 shadow shadow-dark-blue">
+        <div className="flex justify-center text-center">
+          <h1 className="text-2xl font-bold">{title}</h1>
+        </div>
       </div>
     </div>
   );
