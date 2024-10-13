3e479b01f808610d55c6f96e32867fefed35d6a5
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-10-13 14:56:56 -0300

    d

--- backend/models/midia.go
+++ backend/models/midia.go
@@ -4,13 +4,13 @@ import "time"
 
 type (
 	Midia struct {
-		ID               string        `json:"id" bson:"_id"`
-		FileName         string        `json:"file_name" bson:"file_name"`
-		FileOriginalName string        `json:"file_original_name" bson:"file_original_name"`
-		FileUrl          string        `json:"file_url" bson:"file_url"`
-		FileType         string        `json:"file_type" bson:"file_type"`
-		CreatedAt        time.Time     `json:"created_at" bson:"created_at"`
-		UpdatedAt        time.Time     `json:"updated_at" bson:"updated_at"`
-		Duration         time.Duration `json:"duration" bson:"duration"`
+		ID               string    `json:"id" bson:"_id"`
+		FileName         string    `json:"file_name" bson:"file_name"`
+		FileOriginalName string    `json:"file_original_name" bson:"file_original_name"`
+		FileUrl          string    `json:"file_url" bson:"file_url"`
+		FileType         string    `json:"file_type" bson:"file_type"`
+		CreatedAt        time.Time `json:"created_at" bson:"created_at"`
+		UpdatedAt        time.Time `json:"updated_at" bson:"updated_at"`
+		Duration         int       `json:"duration" bson:"duration"`
 	}
 )
