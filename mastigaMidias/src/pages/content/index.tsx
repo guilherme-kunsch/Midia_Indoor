4f9ad600210de0c0f4b6406a34fb134785b36bea
ronanzindev <morellianogm@gmail.com>
2024-10-16 10:27:11 -0300

    fix: cache

--- mastigaMidias/src/pages/content/index.tsx
+++ mastigaMidias/src/pages/content/index.tsx
@@ -1,108 +1,74 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Playlist, Midia } from "../../types/playlist";
-import TextView from "../../components/TextView";
 import mqtt from "mqtt";
 import { fetchPlaylistFromAPI } from "../../utils/playlist";
-import {Cache} from "../../types/cache"
 import { fetchDeviceFromAPI } from "../../utils/device";
-const MQTT_URL = import.meta.env.VITE_MQTT_URL
-const MQTT_USER = import.meta.env.VITE_MQTT_USER
-const MQTT_PASSWORD = import.meta.env.VITE_MQTT_PASSWORD
+import { useCache } from "../../hooks/cache";
+import MidiaRenderer from "../../components/ShowMidias";
+
+const MQTT_URL = import.meta.env.VITE_MQTT_URL;
+const MQTT_USER = import.meta.env.VITE_MQTT_USER;
+const MQTT_PASSWORD = import.meta.env.VITE_MQTT_PASSWORD;
 export const Content = () => {
     const navigate = useNavigate()
     const { playlistId } = useParams();
+    if(!playlistId) {
+        return <>Loading...</>
+    }
     const {search} = useLocation()
     const queryParams = new URLSearchParams(search)
     const deviceId = queryParams.get("device")
-    const [playlist, setPlaylist] = useState<Playlist | null>(null);
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
-    const [cachedUrls, setCachedUrls] = useState<Cache>({});
+    const {cachedUrls,isLoading,playlist,setPlaylist, cacheMidias} = useCache(playlistId)
+    const updateCache = async () => {
+        try {
+          const fetchedPlaylist = await fetchPlaylistFromAPI(playlistId!);
+          setPlaylist(fetchedPlaylist);
+          await caches.delete("midias-" + playlistId!);
 
+          if (fetchedPlaylist.midias && fetchedPlaylist.midias.length > 0) {
+            await cacheMidias(fetchedPlaylist);
+          }
 
-    const cacheMidias = async (playlist: Playlist) => {
-        const cache = await caches.open("midias-"+playlist.id);
-        const newCacheUrls: Cache = {};
+          console.log("Cache atualizado com sucesso!");
+        } catch (error) {
+          console.error("Erro ao atualizar o cache via MQTT:", error);
+        }
+      };
+    const onMessage = async (topic: string, message: Buffer) => {
+        if(topic === playlistId) {
+            const msg = message.toString()
+            if(msg === "update") {
+                updateCache()
+            }
+            if(msg === "delete") {
+                navigate("/")
+            }
+        }
+        if(topic === deviceId) {
+            const msg = message.toString()
+            if(msg === "delete") {
+                navigate("/")
+            }
 
-        const mediaPromises = playlist.midias.map(async (midia) => {
-            const cachedResponse = await cache.match(midia.file_url);
-            if (!cachedResponse) {
-                const fetchResponse = await fetch(import.meta.env.VITE_API_URL + "/midia/file/" + midia.file_name);
-                if (fetchResponse.ok) {
-                    await cache.put(midia.file_url, fetchResponse.clone());
-                    if(midia.file_type === "text") {
-                        const text = await fetchResponse.text()
-                        newCacheUrls[midia.file_url] = text
-                    }else {
-                        const blob = await fetchResponse.blob();
-                        const url = URL.createObjectURL(blob);
-                        newCacheUrls[midia.file_url] = url;
+            if(msg === "update") {
+                const device = await fetchDeviceFromAPI(deviceId)
+                if(device.playlist_id) {
+                    const result = await caches.delete("midias-"+playlistId!)
+                    if(result) {
+                        console.log("cache deletado")
                     }
+                    navigate(`/${device.playlist_id}?device=${device.id}`)
+                    window.location.reload()
 
-                }
-            } else {
-                if(midia.file_type === "text") {
-                    const text = await cachedResponse.text()
-                    newCacheUrls[midia.file_url] = text
-                }else {
-                    const blob = await cachedResponse.blob();
-                    const url = URL.createObjectURL(blob);
-                    newCacheUrls[midia.file_url] = url;
+                } else {
+                    navigate("/")
                 }
             }
-        });
 
-        await Promise.all(mediaPromises);
-        setCachedUrls(newCacheUrls);
-    };
-
-
+        }
+    }
     useEffect(() => {
-        const checkAndLoadCache = async () => {
-            try {
-                setIsLoading(true);
-
-                const cacheExists = await caches.has("midias-"+playlistId!);
-                if (cacheExists) {
-                    const cache = await caches.open("midias-"+playlistId!);
-                    const cachedResponse = await cache.match(playlistId!);
-                    if (cachedResponse) {
-                        const cachedData = await cachedResponse.json();
-                        setPlaylist(cachedData);
-                    }
-                }
-                if (!cacheExists || !playlist) {
-                    const fetchedPlaylist = await fetchPlaylistFromAPI(playlistId!);
-                    setPlaylist(fetchedPlaylist);
-
-                    const cache = await caches.open("midias-"+playlistId!);
-                    const response = new Response(JSON.stringify(fetchedPlaylist));
-                    await cache.put(playlistId!, response);
-
-                    await cacheMidias(fetchedPlaylist);
-                }
-            } catch (error) {
-                console.error("Erro ao carregar a playlist ou o cache:", error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        checkAndLoadCache();
-        const updateCache = async () => {
-            try {
-                const fetchedPlaylist = await fetchPlaylistFromAPI(playlistId!);
-                setPlaylist(fetchedPlaylist);
-                const cache = await caches.open("midias-"+playlistId!);
-                const response = new Response(JSON.stringify(fetchedPlaylist));
-                await cache.put(playlistId!, response.clone());
-
-                await cacheMidias(fetchedPlaylist);
-            } catch (error) {
-                console.error("Erro ao atualizar o cache via MQTT:", error);
-            }
-        };
         const client = mqtt.connect(MQTT_URL, { username: MQTT_USER, password: MQTT_PASSWORD })
         client.on("error", console.error)
         client.on('connect', () => {
@@ -110,39 +76,7 @@ export const Content = () => {
             client.subscribe([playlistId!, deviceId!])
         })
 
-        client.on("message", async (topic, message) => {
-            if(topic === playlistId) {
-                const msg = message.toString()
-                if(msg === "update") {
-                    updateCache()
-                }
-                if(msg === "delete") {
-                    navigate("/")
-                }
-            }
-            if(topic === deviceId) {
-                const msg = message.toString()
-                if(msg === "delete") {
-                    navigate("/")
-                }
-
-                if(msg === "update") {
-                    const device = await fetchDeviceFromAPI(deviceId)
-                    if(device.playlist_id) {
-                        const result = await caches.delete("midias-"+playlistId!)
-                        if(result) {
-                            console.log("cache deletado")
-                        }
-                        navigate(`/${device.playlist_id}?device=${device.id}`)
-                        window.location.reload()
-
-                    } else {
-                        navigate("/")
-                    }
-                }
-
-            }
-        })
+        client.on("message",onMessage)
 
         return () =>  {
             client.end(() => console.log("end"))
@@ -177,39 +111,6 @@ export const Content = () => {
         }
     }, [currentIndex, playlist]);
 
-    const renderMidia = (midia: Midia) => {
-        const cached = cachedUrls[midia.file_url];
-        if (!cached) return null;
-        console.log(cached)
-        switch (midia.file_type) {
-            case "image":
-                return (
-                    <div key={midia.id} className="w-full h-full flex justify-center items-center">
-                        <img src={cached} alt="" className="object-contain w-full h-full" />
-                    </div>
-                );
-            case "video":
-                return (
-                    <div key={midia.id} className="w-full h-full flex justify-center items-center">
-                        <video
-                            src={cached}
-                            className="w-full h-full object-contain"
-                            autoPlay
-                            muted
-                            playsInline
-                        />
-                    </div>
-                );
-            case "text":
-                return (
-                    <div key={midia.id} className="w-full h-full flex justify-center items-center">
-                        <TextView content={cached} className="text-center text-white text-3xl" />
-                    </div>
-                );
-            default:
-                return null;
-        }
-    };
     if (isLoading) {
         return <>Loading...</>;
     }
@@ -219,7 +120,7 @@ export const Content = () => {
     }
     return (
         <div className="w-screen h-screen bg-black">
-            {playlist.midias && renderMidia(playlist.midias[currentIndex])}
+            {playlist.midias && <MidiaRenderer cachedUrls={cachedUrls} midia={playlist.midias[currentIndex]} />}
         </div>
     );
 };
